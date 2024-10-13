c09ed3c7253ef8873539cc8fa6d1c72d3a0d42b8
qulx <qulx@1jz.cc>
2024-10-13 23:47:06 +0300

    working condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,47 @@
 //   id: 'targetEl',
 //   override: true
 // }; need.
+
+import { Collapse } from 'flowbite';
+
+// set the target element that will be collapsed or expanded (eg. navbar menu)
+const $targetEl = document.getElementById('targetEl');
+
+// optionally set a trigger element (eg. a button, hamburger icon)
+const $triggerEl = document.getElementById('triggerEl');
+
+// optional options with default values and callback functions
+const options = {
+    onCollapse: () => {
+        console.log('element has been collapsed');
+    },
+    onExpand: () => {
+        console.log('element has been expanded');
+    },
+    onToggle: () => {
+        console.log('element has been toggled');
+    },
+};
+
+// instance options object
+const instanceOptions = {
+  id: 'targetEl',
+  override: true
+};
+
+/*
+ * $targetEl: required
+ * $triggerEl: optional
+ * options: optional
+ * instanceOptions: optional
+ */
+const collapse = new Collapse(
+    $targetEl,
+    $triggerEl,
+    options,
+    instanceOptions
+);
+
+// show the target element
+collapse.expand();
+
